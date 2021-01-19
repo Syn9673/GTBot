561d668f5b1c92c0e279c5f3a1141f5f08b110cd
Alexander9673 <59809478+Alexander9673@users.noreply.github.com>
2021-01-19 19:05:37 +0800

    Add 404 page to allow site building.
    Building the production mode requires the 404 page, and i seem to have forgotten it. Anyways, here it is.

---
+++ web/pages/404.jsx
@@ -0,0 +1,8 @@
+const _404 = () => (
+  <center className='my-9'>
+    <h2 className='text-light'>404 Not Found</h2>
+    <small className='text-light'>This url is not registered, make sure you typed the correct one.</small>
+  </center>
+)
+
+export default _404
